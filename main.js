501609242f8bff8d657f938e56f9e84268eab838
schmidtn-dot <63585843+schmidtn-dot@users.noreply.github.com>
2023-03-24 20:45:02 +0000

    change val

--- main.js
+++ main.js
@@ -130,7 +130,7 @@ const postFXMaterial = new THREE.ShaderMaterial({
 
       void main () {
         // Set the correct position of each plane vertex
-        gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 0.9975);
+        gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 0.998);
 
         // Pass in the correct UVs to the fragment shader
         v_uv = uv;
