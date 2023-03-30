2ee7fef111c5f84768c293153d4d31bce19c9c6e
schmidtn-dot <63585843+schmidtn-dot@users.noreply.github.com>
2023-03-30 23:11:51 +0000

    sensor test

--- main.js
+++ main.js
@@ -283,39 +283,8 @@ function getAccel(){
         if (response == 'granted') {
        // Add a listener to get smartphone orientation 
            // in the alpha-beta-gamma axes (units in degrees)
-            window.addEventListener('deviceorientation',(event) => {
-              orient(event)
-                // Expose each orientation angle in a more readable way
-                rotation_degrees = event.alpha;
-                frontToBack_degrees = event.beta;
-                leftToRight_degrees = event.gamma;
-                
-                // Update velocity according to how tilted the phone is
-                // Since phones are narrower than they are long, double the increase to the x velocity
-                vx = vx + leftToRight_degrees * updateRate*2; 
-                vy = vy + frontToBack_degrees * updateRate;
-                
-                // Update position and clip it to bounds
-                px = px + vx*.5;
-                if (px > 98 || px < 0){ 
-                    px = Math.max(0, Math.min(98, px)) // Clip px between 0-98
-                    vx = 0;
-                }
-
-                py = py + vy*.5;
-                if (py > 98 || py < 0){
-                    py = Math.max(0, Math.min(98, py)) // Clip py between 0-98
-                    vy = 0;
-                }
-                
-                let x = ((px / window.innerWidth) * 2 - 1)
-                let y = ((1 - py / window.innerHeight) * 2 - 1) 
-                //document.querySelector(".number-text").innerText = "x: " + x + " y: " + y
-                postFXMesh.material.uniforms.orientationX.value = x
-                postFXMesh.material.uniforms.orientationX.value = y
-                document.querySelector(".text").innerText = "x: " + x
-
-            });
+           orient()
+           
         }
     });
   }
@@ -383,36 +352,39 @@ function maus(){
     })
   }
 
-  function orinet(event){
-     // Expose each orientation angle in a more readable way
-     rotation_degrees = event.alpha;
-     frontToBack_degrees = event.beta;
-     leftToRight_degrees = event.gamma;
-     
-     // Update velocity according to how tilted the phone is
-     // Since phones are narrower than they are long, double the increase to the x velocity
-     vx = vx + leftToRight_degrees * updateRate*2; 
-     vy = vy + frontToBack_degrees * updateRate;
-     
-     // Update position and clip it to bounds
-     px = px + vx*.5;
-     if (px > 98 || px < 0){ 
-         px = Math.max(0, Math.min(98, px)) // Clip px between 0-98
-         vx = 0;
-     }
+  function orinet(){
+    window.addEventListener('deviceorientation',(event) => {
+      orient(event)
+        // Expose each orientation angle in a more readable way
+        rotation_degrees = event.alpha;
+        frontToBack_degrees = event.beta;
+        leftToRight_degrees = event.gamma;
+        
+        // Update velocity according to how tilted the phone is
+        // Since phones are narrower than they are long, double the increase to the x velocity
+        vx = vx + leftToRight_degrees * updateRate*2; 
+        vy = vy + frontToBack_degrees * updateRate;
+        
+        // Update position and clip it to bounds
+        px = px + vx*.5;
+        if (px > 98 || px < 0){ 
+            px = Math.max(0, Math.min(98, px)) // Clip px between 0-98
+            vx = 0;
+        }
 
-     py = py + vy*.5;
-     if (py > 98 || py < 0){
-         py = Math.max(0, Math.min(98, py)) // Clip py between 0-98
-         vy = 0;
-     }
-     
-     let x = ((px / window.innerWidth) * 2 - 1)
-     let y = ((1 - py / window.innerHeight) * 2 - 1) 
-     //document.querySelector(".number-text").innerText = "x: " + x + " y: " + y
-     postFXMesh.material.uniforms.orientationX.value = x
-     postFXMesh.material.uniforms.orientationX.value = y
-     document.querySelector(".text").innerText = "x: " + x
+        py = py + vy*.5;
+        if (py > 98 || py < 0){
+            py = Math.max(0, Math.min(98, py)) // Clip py between 0-98
+            vy = 0;
+        }
+        
+        let x = ((px / window.innerWidth) * 2 - 1)
+        let y = ((1 - py / window.innerHeight) * 2 - 1) 
+        //document.querySelector(".number-text").innerText = "x: " + x + " y: " + y
+        postFXMesh.material.uniforms.orientationX.value = x
+        postFXMesh.material.uniforms.orientationX.value = y
+        document.querySelector(".text").innerText = "x: " + x
+    });
   }
 
 
