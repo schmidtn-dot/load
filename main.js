e08d4a721c46a230f946bee89c4321842f508caf
schmidtn-dot <63585843+schmidtn-dot@users.noreply.github.com>
2023-03-30 23:06:45 +0000

    sensor test

--- main.js
+++ main.js
@@ -240,7 +240,7 @@ const postFXMaterial = new THREE.ShaderMaterial({
         //vec4 inputColor = texture2D(sampler, v_uv + vec2(.00));
           float a = snoise(vec3(v_uv * 5.1, time * 0.1)) * 0.0032;
         float b = snoise(vec3(v_uv * 5.1, time * 0.1 + 100.0)) * 0.0032;
-        vec4 inputColor = texture2D(sampler, v_uv + vec2(a * 0.05, b * 0.05) + vec2(orientationX, orientationY));
+        vec4 inputColor = texture2D(sampler, v_uv + vec2(a * 0.05, b * 0.05) + vec2(orientationX, orientationY) * 2.0);
       
         // Set the correct color of each pixel that makes up the plane
         gl_FragColor = vec4(inputColor * 1.0);
