3f65f0313265196d11a2651e129a1537340cb5e1
schmidtn-dot <63585843+schmidtn-dot@users.noreply.github.com>
2023-03-31 12:24:05 +0000

    add orient

--- main.js
+++ main.js
@@ -319,8 +319,8 @@ function getAccel(){
               let x = frontToBack_degrees * 0.01
               let y = leftToRight_degrees * 0.01
               //document.querySelector(".number-text").innerText = "x: " + x + " y: " + y
-              postFXMesh.material.uniforms.orientationX.value = y * -1
-              postFXMesh.material.uniforms.orientationY.value = x 
+              postFXMesh.material.uniforms.orientationX.value = y 
+              postFXMesh.material.uniforms.orientationY.value = x * -1
             });
         
          
