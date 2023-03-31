f26efd8e597d6c65c9c863adcc323f819d88c3db
schmidtn-dot <63585843+schmidtn-dot@users.noreply.github.com>
2023-03-31 10:40:38 +0000

    add orient

--- main.js
+++ main.js
@@ -240,7 +240,7 @@ const postFXMaterial = new THREE.ShaderMaterial({
         //vec4 inputColor = texture2D(sampler, v_uv + vec2(.00));
           float a = snoise(vec3(v_uv * 5.1, time * 0.1)) * 0.0032;
         float b = snoise(vec3(v_uv * 5.1, time * 0.1 + 100.0)) * 0.0032;
-        vec4 inputColor = texture2D(sampler, v_uv + vec2(a * 0.05, b * 0.05) + vec2(orientationX, orientationY) * 0.005);
+        vec4 inputColor = texture2D(sampler, v_uv + vec2(a * 0.05, b * 0.05) + vec2(orientationX, orientationY) * 0.01);
       
         // Set the correct color of each pixel that makes up the plane
         gl_FragColor = vec4(inputColor * 1.0);
@@ -319,7 +319,7 @@ function getAccel(){
               let y = (py * 0.01  - 0.5) * 2
               //document.querySelector(".number-text").innerText = "x: " + x + " y: " + y
               postFXMesh.material.uniforms.orientationX.value = y
-              postFXMesh.material.uniforms.orientationY.value = x
+              postFXMesh.material.uniforms.orientationY.value = -x
               document.querySelector(".text").innerText = "x " + x
             });
         
