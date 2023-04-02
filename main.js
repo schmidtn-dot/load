cc5f55e84e9e524b793ce5654ba286ef9d41beea
schmidtn-dot <63585843+schmidtn-dot@users.noreply.github.com>
2023-04-02 19:25:00 +0000

    cideotest

--- main.js
+++ main.js
@@ -150,7 +150,7 @@ videoMaterial.alphaMap.invert = true;
 const videoMesh = new THREE.Mesh(videoGeometry, videoMaterial);
 
 if (window.location.href.indexOf("orientation") > -1) {
-  videoMesh.material.opacity = 0.05
+  videoMesh.material.opacity = 0.5
   scene.add(videoMesh);
 } 
  
