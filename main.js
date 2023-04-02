592520cf6bfc653c0162bec231e47788e7eeb796
schmidtn-dot <63585843+schmidtn-dot@users.noreply.github.com>
2023-04-02 14:40:31 +0000

    btn

--- main.js
+++ main.js
@@ -88,7 +88,7 @@ let labelTextureCanvas
   const widthDelta = labelTextureCanvas.width / textWidth 
   const fontSize = relativeFontSize * widthDelta 
   labelTextureCtx.font = `${fontSize}px Helvetica`
-  labelTextureCtx.fillStyle = "white"
+  labelTextureCtx.fillStyle = "blue"
   labelTextureCtx.fillText(LABEL_TEXT, labelTextureCanvas.width / 2, labelTextureCanvas.height / 2)
 }
 // Create a material with our programmaticaly created text texture as input
