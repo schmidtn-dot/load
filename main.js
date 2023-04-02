846797fd1cb7aa4deeabc3537aaac4f2294e4b3f
schmidtn-dot <63585843+schmidtn-dot@users.noreply.github.com>
2023-04-02 13:59:44 +0000

    btn

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ setTimeout(()=>{
   }
 },4000)
 
-if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
+if(navigator.userAgentData.mobile){
   console.log("mobile")
   document.querySelector("#accelPermsButton").style.display = "flex"
   document.querySelector("#accelPermsButton").style.opacity  = 1
